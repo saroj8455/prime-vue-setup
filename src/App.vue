649035cace8263527e3b92335202ccced6c0937d
Saroj Padhan <40264980+saroj8455@users.noreply.github.com>
2024-12-29 23:01:44 +0530

    setup prime card with header image

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,14 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router';
 import Welcome from './components/Welcome.vue';
+import PrimeCard from './components/PrimeCard.vue';
 </script>
 
 <template>
 	<main>
 		<h2 class="text-center">Prime Vue Setup</h2>
 		<Welcome />
+		<PrimeCard />
 	</main>
 	<RouterView />
 </template>
---
+++ src/components/PrimeCard.vue
@@ -0,0 +1,52 @@
+<script setup>
+import { Button } from 'primevue';
+import Card from 'primevue/card';
+</script>
+
+<template>
+	<Card>
+		<template #title>Simple Card</template>
+		<template #content>
+			<p class="m-0">
+				Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore sed
+				consequuntur error repudiandae numquam deserunt quisquam repellat libero
+				asperiores earum nam nobis, culpa ratione quam perferendis esse,
+				cupiditate neque quas!
+			</p>
+		</template>
+	</Card>
+	<Card style="width: 25rem; overflow: hidden; margin-top: 0.5rem">
+		<template #header>
+			<div class="custom-header">
+				<img
+					alt="user header"
+					src="https://primefaces.org/cdn/primevue/images/card-vue.jpg"
+				/>
+			</div>
+		</template>
+		<template #title>Advanced Card</template>
+		<template #subtitle>Card subtitle</template>
+		<template #content>
+			<p class="m-0">
+				Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore sed
+				consequuntur error repudiandae numquam deserunt quisquam repellat libero
+				asperiores earum nam nobis, culpa ratione quam perferendis esse,
+				cupiditate neque quas!
+			</p>
+		</template>
+		<template #footer>
+			<div class="flex gap-4 mt-1">
+				<Button label="Cancel" severity="secondary" outlined class="w-full" />
+				<Button label="Save" class="w-full" />
+			</div>
+		</template>
+	</Card>
+</template>
+
+<style scoped>
+.custom-header img {
+	width: 100%;
+	object-fit: contain;
+	aspect-ratio: 16/9;
+}
+</style>
